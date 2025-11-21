76e35ea352b4041785c18ff40fce209da52305c3
VerzilinSeva <144003819+VerzilinSeva@users.noreply.github.com>
2025-11-21 18:56:29 +0300

    6. Обеспечить перегрузку контруктора для нетривиальных примеров (строка, JSON и тд)

--- задание 1/Client.java
+++ задание 1/Client.java
@@ -1,5 +1,11 @@
 package org.example;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Client {
     private String name;
     private String address;
@@ -15,6 +21,49 @@ public class Client {
         this.contactPerson = validateContactPerson(contactPerson);
         this.inn = validateINN(inn);
         this.ogrn = validateOGRN(ogrn);
+    }
+
+    public Client(String data){
+        if (data == null || data.trim().isEmpty()) {
+            throw new IllegalArgumentException("Строка с данными пуста");
+        }
+        String[] dataArray = data.split(";");
+        if (dataArray.length != 6) {
+            throw new IllegalArgumentException(
+                    "Строка должна содержать 6 параметров(имя организации, адрес," +
+                            " телефон, контактное лицо, ИНН, ОГРН)"
+            );
+        }
+        else {
+            this.name = validateName(dataArray[0]);
+            this.address = validateAddress(dataArray[1]);
+            this.phone = validatePhone(dataArray[2]);
+            this.contactPerson = validateContactPerson(dataArray[3]);
+            this.inn = validateINN(dataArray[4]);
+            this.ogrn = validateOGRN(dataArray[5]);
+        }
+    }
+
+    public Client(String jsonFilePath, boolean isJsonFile) throws IOException {
+        if (!isJsonFile) {
+            throw new IllegalArgumentException("Для CSV используйте другой конструктор");
+        }
+
+        try (FileReader reader = new FileReader(jsonFilePath)) {
+            Gson gson = new GsonBuilder().create();
+
+            Client temp = gson.fromJson(reader, Client.class);
+
+            this.name = validateName(temp.name);
+            this.address = validateAddress(temp.address);
+            this.phone = validatePhone(temp.phone);
+            this.contactPerson = validateContactPerson(temp.contactPerson);
+            this.inn = validateINN(temp.inn);
+            this.ogrn = validateOGRN(temp.ogrn);
+
+        } catch (JsonSyntaxException e) {
+            throw new IllegalArgumentException("Ошибка в формате JSON", e);
+        }
     }
 
     public static String validateName(String name) {
@@ -76,5 +125,4 @@ public class Client {
 
     public String getOgrn() { return ogrn; }
     public void setOgrn(String ogrn) { this.ogrn = validateOGRN(ogrn); }
-
 }
